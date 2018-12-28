d2f060d99bf3df06eb8539244d98ffb7ff393830
hustfc <spfanc@gmail.com>
2018-12-28 11:57:52 +0800

    创建地图 ，绑定父方法
    中午吃饭版本

--- tank/Assets/Scripts/MapCeation.cs
+++ tank/Assets/Scripts/MapCeation.cs
@@ -6,5 +6,20 @@ public class MapCeation : MonoBehaviour
 {
     public GameObject[] item; //定义存放gameobject的数组
     //0.老家 1.墙 2.障碍 3.出生效果 4.河流 5.草 6.空气墙
-
+    private void Awake()
+    {
+        //实例化老家
+        CreateItem(item[0], new Vector3(0, -8, 0), Quaternion.identity);//物体 坐标 无旋转
+        //墙围着老家
+        CreateItem(item[1], new Vector3(-1, -8, 0), Quaternion.identity);
+        CreateItem(item[1], new Vector3(1, -8, 0), Quaternion.identity);
+        CreateItem(item[1], new Vector3(0, -7, 0), Quaternion.identity);
+        CreateItem(item[1], new Vector3(-1, -7, 0), Quaternion.identity);
+        CreateItem(item[1], new Vector3(1, -7, 0), Quaternion.identity);
+    }
+    private void CreateItem(GameObject createGameObject, Vector3 createPosion, Quaternion createRotaion)
+    {
+        GameObject itemGo = Instantiate(createGameObject, createPosion, createRotaion);
+        itemGo.transform.SetParent(gameObject.transform);
+    }
 }
